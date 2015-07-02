d60fad341cde1ac5c598ca4b36aefb0a000fdda4
Youssef Kababe <hello@youssefkababe.com>
2015-07-02 17:57:46 +0000

    Install emberfire

--- config/environment.js
+++ config/environment.js
@@ -4,6 +4,8 @@ module.exports = function(environment) {
   var ENV = {
     modulePrefix: 'firechat',
     environment: environment,
+    contentSecurityPolicy: { 'connect-src': "'self' wss://*.firebaseio.com" },
+    firebase: 'https://firechatdb.firebaseio.com/',
     baseURL: '/',
     locationType: 'auto',
     EmberENV: {
