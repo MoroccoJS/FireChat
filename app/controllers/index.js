2ee2cde00ad2ea2b70edb71aa24b8b6fd6a2f07a
Youssef Kababe <hello@youssefkababe.com>
2015-07-02 20:39:21 +0000

    Prompt user for a username

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -3,6 +3,7 @@ import Ember from 'ember';
 export default Ember.ArrayController.extend({
   sortProperties: ['timestamp'],
   sortAscending: false, // sorts post by timestamp
+  username: prompt("Please choose a username"),
   actions: {
     sendMessage: function() {
       var newMessage = this.store.createRecord('message', {
