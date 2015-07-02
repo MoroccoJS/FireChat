8176b9901f4adab4841eba8e32535a277c1d528b
Youssef Kababe <hello@youssefkababe.com>
2015-07-02 23:49:38 +0000

    Add pleasejs to generate username color

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -4,13 +4,14 @@ export default Ember.ArrayController.extend({
   sortProperties: ['timestamp'],
   sortAscending: false, // sorts post by timestamp
   username: prompt("Please choose a username"),
+  color: Please.make_color(),
   actions: {
     sendMessage: function() {
       if (this.get('body')) {
         var newMessage = this.store.createRecord('message', {
           username: this.get('username'),
           body: this.get('body'),
-          color: "#A2D72F",
+          color: this.get('color'),
           timestamp: new Date().getTime()
         });
 
--- app/models/message.js
+++ app/models/message.js
@@ -3,5 +3,6 @@ import DS from 'ember-data';
 export default DS.Model.extend({
   username: DS.attr('string'),
   body: DS.attr('string'),
+  color: DS.attr('string'),
   timestamp: DS.attr('number')
 });
